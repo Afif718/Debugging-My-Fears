c845961fc56c5eec4cb7fd628a4e4fc7ee6f07e2
Afif718 <afifahmad718.com>
2025-04-23 21:57:25 +0600

    Updated at 2025-04-23 21:57:25

---
+++ Module_10/string_initialization.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+    //char* a = "Afif is unstoppable and he believes in himself!";
+    char a[5]= "Afif"; //always get one extra space for null character '\0'
+    printf("%s", a);
+    return 0;
+}
