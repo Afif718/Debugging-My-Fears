cda241564284e8772d1b7ed963efe1861db3e5e6
Afif718 <afifahmad718.com>
2025-05-04 23:42:56 +0600

    Updated at 2025-05-04 23:42:56

--- Module_12/sum_of_two_vals_equal_to_x.c
+++ Module_12/sum_of_two_vals_equal_to_x.c
@@ -1,11 +1,32 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 int main(){
 
-    int arr[] = {2,7,5,10,3};
-    
-    for(int i=0; i<sizeof(arr); i++){
-        printf("%d", arr[i]);
+    int arr_size;
+    scanf("%d", &arr_size);
+
+    int arr[arr_size];
+   
+    for(int i=0; i<arr_size; i++){
+        scanf("%d", &arr[i]);
     }
+
+    int val;
+    scanf("%d", &val);
+
+    for(int i=0; i<arr_size; i++){
+        //printf("%d ", arr[i]);
+        for(int j=i+1; j<arr_size; j++){
+            
+
+            if(arr[i] + arr[j] == val){
+                printf("%d + %d = %d\n", arr[i], arr[j], arr[i]+arr[j]);
+                //equal_x = true;
+            }
+        }
+        //printf("\n");
+    }
+
     return 0;
 }
