33bac016acc07faffc18a3d7e5b347f209383e9a
Afif718 <afifahmad718.com>
2025-05-05 00:50:45 +0600

    Updated at 2025-05-05 00:50:44

---
+++ Module_12/selection_sort_ascending.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+
+    int arr_size;
+    scanf("%d", &arr_size);
+
+    int arr[arr_size];
+   
+    for(int i=0; i<arr_size; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int temp;
+    for(int i=0; i<arr_size; i++){
+        for(int j=i+1; j<arr_size; j++){
+            //(i>j) 20>10 --> true then swap 
+            //ascending order 10 20 30 40 50
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        printf("%d ", arr[i]);
+    }
+
+    
+    return 0;
+}
--- Module_12/test.c
+++ Module_12/test.c
@@ -15,18 +15,27 @@ int main(){
     int val;
     scanf("%d", &val);
 
+    bool flag_val = false;
+
     for(int i=0; i<arr_size; i++){
         //printf("%d ", arr[i]);
         for(int j=i+1; j<arr_size; j++){
-            printf("%d\n", arr[j]);
+            //printf("%d %d\n", arr[i], arr[j]);
 
-            // if(arr[i] + arr[j] == val){
-            //     printf("%d + %d = %d\n", arr[i], arr[j], arr[i]+arr[j]);
-            //     //equal_x = true;
-            // }
+            if(arr[i] + arr[j] == val){
+                printf("%d + %d = %d\n", arr[i], arr[j], arr[i]+arr[j]);
+                //equal_x = true;
+                //printf("YES\n");
+                flag_val = true;
+            }
         }
         //printf("\n");
     }
 
+    if(flag_val){
+        printf("Yes\n");
+    } else{
+        printf("No\n");
+    }
     return 0;
 }
