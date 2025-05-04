e78277b4f1be3a85954a393a97d54ab931125951
Afif718 <afifahmad718.com>
2025-05-05 01:19:13 +0600

    Updated at 2025-05-05 01:19:12

--- Module_12/selection_sort_ascending.c
+++ Module_12/selection_sort_ascending.c
@@ -12,9 +12,9 @@ int main(){
         scanf("%d", &arr[i]);
     }
 
-    int temp;
     for(int i=0; i<arr_size; i++){
         for(int j=i+1; j<arr_size; j++){
+            //choto hole boro (>)
             //(i>j) 20>10 --> true then swap 
             //ascending order 10 20 30 40 50
             if(arr[i]>arr[j]){
@@ -23,6 +23,10 @@ int main(){
                 arr[j] = temp;
             }
         }
+    }
+
+       //print the sorted array in ascending order
+       for(int i=0; i<arr_size; i++){
         printf("%d ", arr[i]);
     }
 
--- Module_12/selection_sort_decending.c
+++ Module_12/selection_sort_decending.c
@@ -12,17 +12,22 @@ int main(){
         scanf("%d", &arr[i]);
     }
 
-    int temp;
     for(int i=0; i<arr_size; i++){
         for(int j=i+1; j<arr_size; j++){
+            //boro hole choto (<)
             //i<j (10<20) --> true then swap 
             //decending order 50 40 30 20 10
-            if(arr[i]>arr[j]){
+            if(arr[i]<arr[j]){
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
+       
+    }
+
+    //print the sorted array in decending order
+    for(int i=0; i<arr_size; i++){
         printf("%d ", arr[i]);
     }
 
