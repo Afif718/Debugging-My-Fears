d31b4dc8ba6d1782fe25e73b4813eb834b07264a
Afif718 <afifahmad718.com>
2025-05-27 01:18:34 +0600

    Updated at 2025-05-27 01:18:34

--- C++ For DSA/1_Module_1/input.cpp
+++ C++ For DSA/1_Module_1/input.cpp
@@ -11,12 +11,18 @@ int main(){
 
     //if i use it "using namespace std;" i dont have to writ std:: every time
     cin >> x >> a >> d;
-    cout <<x <<std::endl << a <<std::endl <<d;
+    cout <<x <<std::endl << a <<std::endl <<d <<std::endl;
 
     //ascii value of character in c++
-    char a = 'A';
-    int ascii_a = a;
-    cout<< ascii_a <<endl;
+    char z = 'A';
+    int ascii_a = z;
+    cout<< "ASCII Value of " << z << " is " <<  ascii_a <<endl;
+
+    //print ascii value using type-casting
+    cout << "ASCII Value of " << z << " is " << (int) z <<endl;
+
+    int v = 65;
+    cout <<"Convert int value to Char " << x << " is " << (char)v;
 
     return 0;
 }
