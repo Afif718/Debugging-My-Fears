59b91046967d48a8ba9dd7ae0cd6fef0a6ba0314
Afif718 <afifahmad718.com>
2025-05-27 01:00:04 +0600

    Updated at 2025-05-27 01:00:04

---
+++ C++ For DSA/print.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+
+    int x = 10;
+    char c = 'A';
+    double n = 3.14;
+
+    std :: cout << "Hello World" <<std::endl;
+    std :: cout << x << " " << c << " " << n <<std::endl;
+    std :: cout << "Pi Number is " << n << ".";
+
+    return 0;
+}
