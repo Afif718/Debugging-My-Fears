2150113b92f11b9f87c42c2b7655a7462f5fb299
Afif718 <afifahmad718.com>
2025-05-27 01:10:54 +0600

    Updated at 2025-05-27 01:10:54

---
+++ C++ For DSA/1_Module_1/input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x;
+    char a;
+    double d;
+    // std:: cin>> x;
+    // std::cout<< x;
+
+    //if i use it "using namespace std;" i dont have to writ std:: every time
+    cin >> x >> a >> d;
+    cout <<x <<std::endl << a <<std::endl <<d;
+
+    return 0;
+}
