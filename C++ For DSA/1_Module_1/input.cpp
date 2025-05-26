856de5ea72dc0cbe9193c2d4e69f626f3f96b82a
Afif718 <afifahmad718.com>
2025-05-27 01:12:59 +0600

    Updated at 2025-05-27 01:12:59

--- C++ For DSA/1_Module_1/input.cpp
+++ C++ For DSA/1_Module_1/input.cpp
@@ -13,5 +13,10 @@ int main(){
     cin >> x >> a >> d;
     cout <<x <<std::endl << a <<std::endl <<d;
 
+    //ascii value of character in c++
+    char a = 'A';
+    int ascii_a = a;
+    cout<< ascii_a <<endl;
+
     return 0;
 }
