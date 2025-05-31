5cc4bafe73990ead100d65d3119070816e1bc8fa
Afif718 <afifahmad718.com>
2025-05-31 19:26:24 +0600

    Updated at 2025-05-31 19:26:24

---
+++ C++ For DSA/01_Module_1/1_8_string.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int x;
+    // cin >> x;
+
+    char s[100];
+    //string without space
+    // cin >> s;
+    // cout << s <<endl;
+
+    //string with space inbetween
+    fgets(s, 100, stdin);
+    // cin.ignore();
+    // cin.getline(s, 100);
+    cout << s << endl;
+
+    //we can take string like normal variables int double
+    // string s;
+    // cin >> s;
+    // cout << s <<endl;
+
+    return 0;
+}
