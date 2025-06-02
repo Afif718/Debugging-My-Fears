c807b467e431f8c119baa0bc65d0755274146a8f
Afif718 <afifahmad718.com>
2025-06-02 19:11:04 +0600

    Updated at 2025-06-02 19:11:03

--- 2_C++ For DSA/05_Module_3.5_Practice_Day_2/7_dynamic_objects.cpp
+++ 2_C++ For DSA/05_Module_3.5_Practice_Day_2/7_dynamic_objects.cpp
@@ -1,7 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+Cricketer
+{
+	jersey_no;
+	country;
+}
+*/
+
+class Cricketer{
+    public:
+    int jersey_no;
+    string country;
+};
+
 int main(){
     
+    //dynamic objects
+    Cricketer* dhoni = new Cricketer();
+    Cricketer* kohli = new Cricketer();
+    
+    dhoni->jersey_no = 45;
+    dhoni->country = "India";
+
+    kohli->jersey_no = dhoni->jersey_no;
+    kohli->country = dhoni->country;
+
+    delete dhoni;
+
+    cout << kohli->jersey_no << " " << kohli->country;
+
+
     return 0;
 }
