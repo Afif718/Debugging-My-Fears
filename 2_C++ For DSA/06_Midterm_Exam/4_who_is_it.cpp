69e269775bf09c4cfcce56375e3e72851bc59a07
Afif718 <afifahmad718.com>
2025-06-02 21:48:32 +0600

    Updated at 2025-06-02 21:48:31

--- 2_C++ For DSA/06_Midterm_Exam/4_who_is_it.cpp
+++ 2_C++ For DSA/06_Midterm_Exam/4_who_is_it.cpp
@@ -1,31 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Student{
-    public:
+class Student {
+public:
     int id;
     char name[101];
     char section;
     int total_marks;
 };
 
-
-int main(){
+int main() {
+    int num_of_times;
+    cin >> num_of_times;
     
-    Student sakib;
-    Student rakib;
-    Student akib;
+    for (int i = 1; i <= num_of_times; i++) {
+        Student sakib;
+        Student rakib;
+        Student akib;
 
-    cin >> sakib.id >> sakib.name >> sakib.section >> sakib.total_marks;
-    cin >> rakib.id >> rakib.name >> rakib.section >> rakib.total_marks;
-    cin >> akib.id >> akib.name >> akib.section >> akib.total_marks;
+        cin >> sakib.id >> sakib.name >> sakib.section >> sakib.total_marks;
+        cin >> rakib.id >> rakib.name >> rakib.section >> rakib.total_marks;
+        cin >> akib.id >> akib.name >> akib.section >> akib.total_marks;
 
-    if((sakib.total_marks >= rakib.total_marks) && (sakib.total_marks >= akib.total_marks)){
-        cout << sakib.id << sakib.name << sakib.section << sakib.total_marks;
-    } else if((rakib.total_marks >= sakib.total_marks) && (rakib.total_marks >= akib.total_marks)){
-        cout << rakib.id << rakib.name << rakib.section << rakib.total_marks;
-    } else{
-        cout << akib.id << akib.name << akib.section << akib.total_marks;
+        Student top = sakib;
+
+        if (rakib.total_marks > top.total_marks) {
+            top = rakib;
+        } else if (rakib.total_marks == top.total_marks && rakib.id < top.id) {
+            top = rakib;
+        }
+
+        if (akib.total_marks > top.total_marks) {
+            top = akib;
+        } else if (akib.total_marks == top.total_marks && akib.id < top.id) {
+            top = akib;
+        }
+
+        cout << top.id << " " << top.name << " " << top.section << " " << top.total_marks << endl;
     }
 
     return 0;
