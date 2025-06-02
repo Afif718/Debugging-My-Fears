5b4dfe443e03c62063ccfd4b50cd34a0e9bec83f
Afif718 <afifahmad718.com>
2025-06-02 18:44:21 +0600

    Updated at 2025-06-02 18:44:20

---
+++ 2_C++ For DSA/05_Module_3.5_Practice_Day_2/5_sorting_function.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr_size;
+    cin >> arr_size;
+
+    int arr[arr_size];
+    for(int i=0; i<arr_size; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+arr_size);
+
+    for(int i=0; i<arr_size; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+//problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/H
