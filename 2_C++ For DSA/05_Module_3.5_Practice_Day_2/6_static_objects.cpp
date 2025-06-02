8e54e7564e9b3cff121049b4ee31ab0e62c17c8b
Afif718 <afifahmad718.com>
2025-06-02 19:11:51 +0600

    Updated at 2025-06-02 19:11:50

--- 2_C++ For DSA/05_Module_3.5_Practice_Day_2/6_static_objects.cpp
+++ 2_C++ For DSA/05_Module_3.5_Practice_Day_2/6_static_objects.cpp
@@ -35,3 +35,18 @@ int main(){
 
     return 0;
 }
+
+/*
+Question: Create three static objects with the help of the constructor of the following class.
+Student
+{
+	name;
+	roll;
+	section;
+	math_marks;
+	cls;
+}
+Then compare those 3 objects and print who got the highest math_marks and print his/her name.
+
+
+*/
