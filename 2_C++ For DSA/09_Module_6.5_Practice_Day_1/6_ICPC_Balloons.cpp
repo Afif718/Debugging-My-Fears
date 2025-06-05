24444aff152910033b15e6d82ccc307b8b94d1a8
Afif718 <afifahmad718.com>
2025-06-06 02:16:04 +0600

    Updated at 2025-06-06 02:16:03

---
+++ 2_C++ For DSA/09_Module_6.5_Practice_Day_1/6_ICPC_Balloons.cpp
@@ -0,0 +1,425 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int num_of_test_cases;
+    cin >> num_of_test_cases; 
+    
+    for(int i=1; i<=num_of_test_cases; i++){
+
+        int length_of_the_string;
+        cin >> length_of_the_string;
+        //cin.ignore();
+
+        string s;
+        
+
+        int count_a = 0;
+        int count_b = 0;
+        int count_c = 0;
+        int count_d = 0;
+        int count_e = 0;
+        int count_f = 0;
+        int count_g = 0;
+        int count_h = 0;
+        int count_i = 0;
+        int count_j = 0;
+        int count_k = 0;
+        int count_l = 0;
+        int count_m = 0;
+        int count_n = 0;
+        int count_o = 0;
+        int count_p = 0;
+        int count_q = 0;
+        int count_r = 0;
+        int count_s = 0;
+        int count_t = 0;
+        int count_u = 0;
+        int count_v = 0;
+        int count_w = 0;
+        int count_x = 0;
+        int count_y = 0;
+        int count_z = 0;
+
+        //if (s.length() == 1) return 2;
+
+        if(length_of_the_string !=0){
+            cin >> s;
+
+            for(int i = 0; i<length_of_the_string; i++){
+            if(s[i] == 'A'){
+                count_a ++;
+            }
+
+            if(s[i] == 'B'){
+                count_b ++;
+            }
+
+            if(s[i] == 'C'){
+                count_c ++;
+            }
+
+            if(s[i] == 'D'){
+                count_d ++;
+            }
+
+            if(s[i] == 'E'){
+                count_e ++;
+            }
+
+            if(s[i] == 'F'){
+                count_f ++;
+            }
+
+            if(s[i] == 'G'){
+                count_g ++;
+            }
+            if(s[i] == 'H'){
+                count_h ++;
+            }
+            if(s[i] == 'I'){
+                count_i ++;
+            }
+            if(s[i] == 'J'){
+                count_j ++;
+            }
+            if(s[i] == 'K'){
+                count_k ++;
+            }
+            if(s[i] == 'L'){
+                count_l ++;
+            }
+            if(s[i] == 'M'){
+                count_m ++;
+            }
+
+            if(s[i] == 'N'){
+                count_n ++;
+            }
+
+            if(s[i] == 'O'){
+                count_o ++;
+            }
+
+            if(s[i] == 'P'){
+                count_p ++;
+            }
+            if(s[i] == 'Q'){
+                count_q ++;
+            }
+            if(s[i] == 'R'){
+                count_r ++;
+            }
+            if(s[i] == 'S'){
+                count_s ++;
+            }
+            if(s[i] == 'T'){
+                count_t ++;
+            }
+            if(s[i] == 'U'){
+                count_u ++;
+            }
+            if(s[i] == 'V'){
+                count_v ++;
+            }
+            if(s[i] == 'W'){
+                count_w ++;
+            }
+            if(s[i] == 'X'){
+                count_x ++;
+            }
+            if(s[i] == 'Y'){
+                count_y ++;
+            }
+            if(s[i] == 'Z'){
+                count_z ++;
+            }
+        }
+
+        //cout << count_a << " " << count_b <<endl;
+
+        int sum_a = 0;
+        int sum_b = 0;
+        int sum_c = 0;
+        int sum_d = 0;
+        int sum_e = 0;
+        int sum_f = 0;
+        int sum_g = 0;
+        int sum_h = 0;
+        int sum_i = 0;
+        int sum_j = 0;
+        int sum_k = 0;
+        int sum_l = 0;
+        int sum_m = 0;
+        int sum_n = 0;
+        int sum_o = 0;
+        int sum_p = 0;
+        int sum_q = 0;
+        int sum_r = 0;
+        int sum_s = 0;
+        int sum_t = 0;
+        int sum_u = 0;
+        int sum_v = 0;
+        int sum_w = 0;
+        int sum_x = 0;
+        int sum_y = 0;
+        int sum_z = 0;
+
+        for(int i=1; i<=count_a; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_a +=2;
+            } else{
+                sum_a += 1;
+            }
+        }
+
+        for(int i=1; i<=count_b; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_b +=2;
+            } else{
+                sum_b += 1;
+            }
+        }
+
+        for(int i=1; i<=count_c; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_c +=2;
+            } else{
+                sum_c += 1;
+            }
+        }
+
+        for(int i=1; i<=count_d; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_d +=2;
+            } else{
+                sum_d += 1;
+            }
+        }
+
+        for(int i=1; i<=count_e; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_e +=2;
+            } else{
+                sum_e += 1;
+            }
+        }
+
+        for(int i=1; i<=count_f; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_f +=2;
+            } else{
+                sum_f += 1;
+            }
+        }
+
+        for(int i=1; i<=count_g; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_g +=2;
+            } else{
+                sum_g += 1;
+            }
+        }
+
+        for(int i=1; i<=count_h; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_h +=2;
+            } else{
+                sum_h += 1;
+            }
+        }
+
+        for(int i=1; i<=count_i; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_i +=2;
+            } else{
+                sum_i += 1;
+            }
+        }
+
+        for(int i=1; i<=count_j; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_j +=2;
+            } else{
+                sum_j += 1;
+            }
+        }
+
+        for(int i=1; i<=count_k; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_k +=2;
+            } else{
+                sum_k += 1;
+            }
+        }
+
+        for(int i=1; i<=count_l; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_l +=2;
+            } else{
+                sum_l += 1;
+            }
+        }
+
+        for(int i=1; i<=count_m; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_m +=2;
+            } else{
+                sum_m += 1;
+            }
+        }
+
+        for(int i=1; i<=count_n; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_n +=2;
+            } else{
+                sum_n += 1;
+            }
+        }
+
+        for(int i=1; i<=count_o; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_o +=2;
+            } else{
+                sum_o += 1;
+            }
+        }
+
+        for(int i=1; i<=count_p; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_p +=2;
+            } else{
+                sum_p += 1;
+            }
+        }
+
+        for(int i=1; i<=count_q; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_q +=2;
+            } else{
+                sum_q += 1;
+            }
+        }
+
+        for(int i=1; i<=count_r; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_r +=2;
+            } else{
+                sum_r += 1;
+            }
+        }
+
+        for(int i=1; i<=count_s; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_s +=2;
+            } else{
+                sum_s += 1;
+            }
+        }
+
+        for(int i=1; i<=count_t; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_t +=2;
+            } else{
+                sum_t += 1;
+            }
+        }
+
+        for(int i=1; i<=count_u; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_u +=2;
+            } else{
+                sum_u += 1;
+            }
+        }
+
+        for(int i=1; i<=count_v; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_v +=2;
+            } else{
+                sum_v += 1;
+            }
+        }
+
+        for(int i=1; i<=count_w; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_w +=2;
+            } else{
+                sum_w += 1;
+            }
+        }
+
+        for(int i=1; i<=count_x; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_x +=2;
+            } else{
+                sum_x += 1;
+            }
+        }
+
+        for(int i=1; i<=count_y; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_y +=2;
+            } else{
+                sum_y += 1;
+            }
+        }
+
+        for(int i=1; i<=count_z; i++){
+            //cout << i <<endl;
+            if(i==1){
+                sum_z +=2;
+            } else{
+                sum_z += 1;
+            }
+        }
+
+       int total_sum = sum_a + sum_b + sum_c + sum_d + sum_e + sum_f + sum_g + sum_h + sum_i + sum_j
+              + sum_k + sum_l + sum_m + sum_n + sum_o + sum_p + sum_q + sum_r + sum_s + sum_t
+              + sum_u + sum_v + sum_w + sum_x + sum_y + sum_z;
+
+
+
+
+        cout << total_sum <<endl;
+
+        } else{
+            cout << 0 << endl;
+            break;
+        }
+
+        
+
+    }
+
+    
+    return 0;
+}
+
+//problem link: https://codeforces.com/contest/1703/problem/B
--- 2_C++ For DSA/09_Module_6.5_Practice_Day_1/test.cpp
+++ 2_C++ For DSA/09_Module_6.5_Practice_Day_1/test.cpp
@@ -1,7 +1,40 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int t; 
+    cin >> t;  // Read the number of test cases
+
+    while (t--) {  // Loop through each test case
+        int n; 
+        cin >> n;  // Read the length of the string s
+
+        string s; 
+        cin >> s;  // Read the string representing order of solved problems
+
+        // Array to keep track of which problems have been solved before
+        // Index 0 corresponds to 'A', 1 to 'B', ..., 25 to 'Z'
+        bool solved[26] = {false};
+
+        int balloons = 0;  // To keep track of total balloons distributed
+
+        // Loop through each character/problem in the string s
+        for (int i = 0; i < n; i++) {
+            // Convert character to index (e.g., 'A' -> 0, 'B' -> 1, etc.)
+            int index = s[i] - 'A';
+
+            // If this is the first time this problem is solved
+            if (!solved[index]) {
+                balloons += 2;      // 2 balloons: one for solving, one for first solve bonus
+                solved[index] = true;  // Mark this problem as solved
+            } else {
+                balloons += 1;      // Already solved before, only 1 balloon awarded now
+            }
+        }
+
+        cout << balloons << endl;  // Output total balloons for this test case
+    }
+
     return 0;
 }
