94809f5641b2401b30d8489929155f3ad08027ea
Afif718 <afifahmad718.com>
2025-06-14 02:58:17 +0600

    Updated at 2025-06-14 02:58:16

--- 2_C++ For DSA/12_Module_8_Final_Exam/3_reverse_it_using_swap_function.cpp
+++ 2_C++ For DSA/12_Module_8_Final_Exam/3_reverse_it_using_swap_function.cpp
@@ -1,35 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Define the Student class with four public attributes
+// Class to store student information
 class Student {
     public:
-    string nm;   // Student's name
-    string cls;  // Student's class
-    string s;    // Student's section
-    string id;   // Student's ID
+    string nm;   // Name
+    string cls;  // Class
+    string s;    // Section
+    string id;   // ID
 };
 
 int main(){
   
     int arr_obj_size;
-    cin >> arr_obj_size;  // Read the number of students
+    cin >> arr_obj_size;  // Input the number of students
 
-    Student arrObj[arr_obj_size];  // Declare an array of Student objects
+    Student arrObj[arr_obj_size];  // Array to hold student records
 
-    // Read input data for each student
+    // Input student details
     for(int i = 0; i < arr_obj_size; i++){
         cin >> arrObj[i].nm >> arrObj[i].cls >> arrObj[i].s >> arrObj[i].id;
     }
 
-    // Reverse the section field and store it in arr_section
-    for(int i =0, j = arr_obj_size-1; i >j; i++){
-      swap(arrObj[i].s, arrObj[j].s);
+    // In-place reversal of the 'section' field (s) only
+    for(int i = 0, j = arr_obj_size - 1; i < j; i++, j--){
+        swap(arrObj[i].s, arrObj[j].s);
     }
 
-
-
-    // Print all updated student data
+    // Output the updated student records
     for(int i = 0; i < arr_obj_size; i++){
         cout << arrObj[i].nm << " " << arrObj[i].cls << " " << arrObj[i].s << " " << arrObj[i].id << endl;
     }
