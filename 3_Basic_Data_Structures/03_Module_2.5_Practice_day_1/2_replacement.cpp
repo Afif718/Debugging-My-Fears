acebaa2eacf4bba8f3b3c11dd1036c1354894257
Afif718 <afifahmad718.com>
2025-06-20 05:54:06 +0600

    Updated at 2025-06-20 05:54:05

---
+++ 3_Basic_Data_Structures/03_Module_2.5_Practice_day_1/2_replacement.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int vector_size;
+    cin >> vector_size;
+
+    vector<int> v(vector_size);
+
+    for(int i=0; i<vector_size; i++){
+        cin >> v[i];
+    }
+
+    for(int i=0; i<vector_size; i++){
+        if(v[i]>0){
+            replace(v.begin(), v.end(), v[i], 1);
+        } else{
+            replace(v.begin(), v.end(), v[i], 2);
+        }
+    }
+
+    for(int x:v){
+        cout << x << " ";
+    }
+    return 0;
+}
