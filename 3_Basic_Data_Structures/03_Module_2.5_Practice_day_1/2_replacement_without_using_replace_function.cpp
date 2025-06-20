35d79f2f7e45deaefbbbaf7f8042457a70483131
Afif718 <afifahmad718.com>
2025-06-20 06:06:26 +0600

    Updated at 2025-06-20 06:06:26

---
+++ 3_Basic_Data_Structures/03_Module_2.5_Practice_day_1/2_replacement_without_using_replace_function.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int vector_size;
+    cin >> vector_size;
+
+    vector<int> v(vector_size);
+
+    for(int i=0; i<vector_size; i++){
+        cin >> v[i];
+    }
+
+    for(int i=0; i<vector_size; i++){
+        if(v[i]>0){
+            v[i] = 1;
+        } else if(v[i]<0){
+            v[i] = 2;
+        }
+    }
+
+    for(int x:v){
+        cout << x << " ";
+    }
+    return 0;
+}
+
+//problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/C
