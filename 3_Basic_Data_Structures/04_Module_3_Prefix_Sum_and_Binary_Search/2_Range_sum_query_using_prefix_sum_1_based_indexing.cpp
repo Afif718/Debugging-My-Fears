7076ff3d15d00828a9e9c0d79b82b9ed015e7cab
Afif718 <afifahmad718.com>
2025-06-22 19:09:45 +0600

    Updated at 2025-06-22 19:09:45

--- 3_Basic_Data_Structures/04_Module_3_Prefix_Sum_and_Binary_Search/2_Range_sum_query_using_prefix_sum_1_based_indexing.cpp
+++ 3_Basic_Data_Structures/04_Module_3_Prefix_Sum_and_Binary_Search/2_Range_sum_query_using_prefix_sum_1_based_indexing.cpp
@@ -19,6 +19,7 @@ int main(){
     vector<long long int> pre(v_size + 1); // Again, 1-based indexing // O(N)
 
     pre[1] = v[1]; // O(1) – First element of prefix sum is same as the input array
+    //prefix sum formula --> pre[i] = pre[i-1] + v[i] *******
     for(int i = 2; i <= v_size; i++){ // O(N)
         pre[i] = pre[i - 1] + v[i]; // O(1) per operation
         // This means pre[i] = v[1] + v[2] + ... + v[i]
@@ -40,10 +41,11 @@ int main(){
 
         if(l == 1){
             // If l == 1, sum is simply pre[r] (sum from 1 to r)
+            //formula when l=1; --> pre[r] ************
             sum = pre[r]; // O(1)
         } else{
             // Otherwise, subtract prefix before l
-            // pre[r] - pre[l-1] gives sum from l to r
+            // pre[r] - pre[l-1] gives sum from l to r (formula********)
             sum = pre[r] - pre[l - 1]; // O(1)
         }
 
