e7b80f789e2b71196f3ea73ee395b49d51f1ecf0
Afif718 <afifahmad718.com>
2025-06-29 02:53:38 +0600

    Updated at 2025-06-29 02:53:37

--- 3_Basic_Data_Structures/07_Module_5_Singly_Linked_List/2_Dynamic_node.cpp
+++ 3_Basic_Data_Structures/07_Module_5_Singly_Linked_List/2_Dynamic_node.cpp
@@ -14,6 +14,18 @@ class Node{
 
 int main(){
     //Dynamic Object
+    //head is a pointer
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+
+    //previously a.next = &b but now we can use
+    head->next = a;
+    a->next = b;
+
+    cout << head->val <<endl; // head is a pointer and it will print first 
+    cout << head->next->val << endl; // it will print a
+    // cout << head->next->next.val << endl;
     
     return 0;
 }
