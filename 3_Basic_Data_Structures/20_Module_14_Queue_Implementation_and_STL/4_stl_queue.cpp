6b3f803039d3f5fb3592804f96c79834b48cd61c
Afif718 <39927237+Afif718@users.noreply.github.com>
2025-07-24 01:45:26 +0600

    Updated at 2025-07-24 01:45:26

--- 3_Basic_Data_Structures/20_Module_14_Queue_Implementation_and_STL/4_stl_queue.cpp
+++ 3_Basic_Data_Structures/20_Module_14_Queue_Implementation_and_STL/4_stl_queue.cpp
@@ -1,22 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+/*
+STL queue<int> Summary:
+Operation       Function    Time Complexity
+Insert          push(val)   O(1)
+Remove          pop()       O(1)
+Access front    front()     O(1)
+Check empty     empty()     O(1)
+Get size        size()      O(1)
+*/
+
+int main() {
+    // Declare a queue of integers using STL
     queue<int> q;
 
     int q_size;
-    cin >> q_size;
+    cin >> q_size; // Input number of elements to insert in the queue
 
-    for(int i=0; i<q_size; i++){
+    for (int i = 0; i < q_size; i++) {
         int val;
         cin >> val;
 
+        // Insert element at the back of the queue
+        // Time Complexity: O(1)
         q.push(val);
     }
 
-    while(!q.empty()){
-        cout << q.front()  << " ";
+    // Loop until the queue becomes empty
+    while (!q.empty()) {
+        // Access the front element
+        // Time Complexity: O(1)
+        cout << q.front() << " ";
+
+        // Remove the front element from the queue
+        // Time Complexity: O(1)
         q.pop();
     }
+
     return 0;
 }
