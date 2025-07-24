ac8c0f7adbade53a2d996044bc35a1926788771b
Afif718 <39927237+Afif718@users.noreply.github.com>
2025-07-24 23:00:20 +0600

    Updated at 2025-07-24 23:00:19

---
+++ 3_Basic_Data_Structures/21_Module_14.5_Practice_Day_1/1_check_same_or_not.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> st;
+    stack<int> st2;
+
+    int st_size;
+    cin >> st_size;
+
+    for(int i=0; i<st_size; i++){
+        int val;
+        cin >> val;
+        st.push(val);
+    }
+
+    int st2_size;
+    cin >> st2_size;
+
+    //input st2 vals in the stack2
+    for(int i=0; i<st2_size; i++){
+        int val;
+        cin >> val;
+        st2.push(val);
+    }
+
+    bool isSame = true;
+
+    if(st.size() == st2.size()){
+        while(!st.empty() && !st2.empty()){
+            if(st.top() != st2.top()){
+                isSame = false;
+                break;
+            }
+
+            st.pop();
+            st2.pop();
+        }
+    } else{
+        cout << "NO\n";
+        return 0;
+    }
+
+    cout << (isSame? "YES\n" : "NO\n");
+    return 0;
+}
+
+/*
+Question: Take two stacks of size N and M as input and check if both of them are the same or not. Don’t use STL stack to solve this problem.
+
+Sample Input
+Sample Output
+5
+10 20 30 40 50
+5
+10 20 30 40 50
+YES
+5
+10 20 30 40 50
+4
+10 20 30 40
+NO
+5
+10 20 30 40 50
+5
+50 40 30 20 10
+NO
+
+
+*/
