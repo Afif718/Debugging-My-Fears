dbbc2a4599eedc405159682488781a4fbf3336f0
Afif718 <39927237+Afif718@users.noreply.github.com>
2025-07-24 23:25:03 +0600

    Updated at 2025-07-24 23:25:03

--- 3_Basic_Data_Structures/21_Module_14.5_Practice_Day_1/1_check_same_or_not_2_stacks_manually.cpp.cpp
+++ 3_Basic_Data_Structures/21_Module_14.5_Practice_Day_1/1_check_same_or_not_2_stacks_manually.cpp.cpp
@@ -1,7 +1,73 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class myStack{
+    public:
+        vector<int> v;
+
+    void push(int val){
+        v.push_back(val);
+    }
+
+    void pop(){
+        if(!v.empty()){
+            v.pop_back();
+        }
+    }
+
+    int top(){
+        return v.back();
+    }
+
+    int size(){
+        return v.size();
+    }
+
+    bool empty(){
+        return v.empty();
+    }
+
+};
+
 int main(){
+
+    myStack st;
+    myStack st2;
+
+    int st_size;
+    cin >> st_size;
+    for(int i=0; i<st_size; i++){
+        int val;
+        cin >> val;
+        st.push(val);
+    }
+
+    int st2_size;
+    cin >> st2_size;
+    for(int i=0; i<st2_size; i++){
+        int val;
+        cin >> val;
+        st2.push(val);
+    }
+
+    bool isSame = true;
+
+    if(st.size() == st2.size()){
+        while(!st.empty() && !st2.empty()){
+            if(st.top() != st2.top()){
+                isSame = false;
+                break;
+            }
+
+            st.pop();
+            st2.pop();
+        }
+    } else{
+        cout << "NO\n";
+        return 0;
+    }
+
+    cout << ((isSame)? "YES\n" : "NO\n");
     
     return 0;
 }
