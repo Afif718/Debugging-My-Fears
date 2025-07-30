e72624f5487daf9082fd7565953a45405e9fc466
Afif718 <39927237+Afif718@users.noreply.github.com>
2025-07-30 23:35:26 +0600

    Updated at 2025-07-30 23:35:26

---
+++ 3_Basic_Data_Structures/28_Module_19_Problem_Solving_using_Binary_Tree/1_find_a_node_in_binary_tree.cpp
@@ -0,0 +1,78 @@
+/************************************************************
+
+    Following is the Binary Tree node structure
+
+    template <typename T>
+    class BinaryTreeNode
+    {
+        public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data)
+        {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+bool isNodePresent(BinaryTreeNode<int> *root, int x) {
+    
+    queue<BinaryTreeNode<int>*> q;
+
+    q.push(root);
+
+    while(!q.empty()){
+        BinaryTreeNode<int>* p = q.front();
+        q.pop();
+
+        if(p->data == x){
+            return true;
+        }
+
+
+        if(p->left){
+            q.push(p->left);
+        }
+
+        if(p->right){
+            q.push(p->right);
+        }
+
+        
+    }
+
+    return false;
+
+}
+
+/*
+https://www.naukri.com/code360/problems/code-find-a-node_5682?leftPanelTabValue=PROBLEM
+
+Problem statement
+For a given Binary Tree of type integer and a number X, find whether a node exists in the tree with data X or not.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= N <= 10^5
+
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec.
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+7
+Sample Output 1:
+true
+Explanation For Output 1:
+Clearly, we can see that 7 is present in the tree. So, the output will be true.
+Sample Input 2:
+2 3 4 -1 -1 -1 -1
+10
+Sample Output 2:
+false
+*/
