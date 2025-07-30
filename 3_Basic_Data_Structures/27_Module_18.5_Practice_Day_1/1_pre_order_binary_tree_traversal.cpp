ae3877d8b14bfe51e4b0fddfbd9024b2b0ecf583
Afif718 <39927237+Afif718@users.noreply.github.com>
2025-07-30 23:13:22 +0600

    Updated at 2025-07-30 23:13:22

--- 3_Basic_Data_Structures/27_Module_18.5_Practice_Day_1/1_pre_order_binary_tree_traversal.cpp
+++ 3_Basic_Data_Structures/27_Module_18.5_Practice_Day_1/1_pre_order_binary_tree_traversal.cpp
@@ -1,43 +1,20 @@
-// Definition for a binary tree node (already provided in most platforms like LeetCode):
-// struct TreeNode {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
-
 class Solution {
 public:
-
-    // This helper function performs recursive preorder traversal of the binary tree
-    void traverse(TreeNode* root, vector<int>& result) {
-        // Base Case: If the current node is NULL, stop the recursion and return
-        if(root == NULL) {
+    vector<int> result;
+    void preorder(TreeNode* root){
+        if(root == NULL){
             return;
         }
 
-        // Step 1: Visit the current node and store its value
-        // In preorder traversal, we visit the root node first
         result.push_back(root->val);
 
-        // Step 2: Recursively traverse the left subtree
-        traverse(root->left, result);
-
-        // Step 3: Recursively traverse the right subtree
-        traverse(root->right, result);
+        preorder(root->left);
+        preorder(root->right);
     }
 
-    // This function will be called by the main program to start the traversal
     vector<int> preorderTraversal(TreeNode* root) {
-        // Vector to store the result of preorder traversal
-        vector<int> result;
+        preorder(root);
 
-        // Start the recursive traversal from the root
-        traverse(root, result);
-
-        // Return the final preorder traversal as a vector
         return result;
     }
 };
