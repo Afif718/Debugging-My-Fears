5c4de1e1ca35ef2c529843fc7ed57b11f9400e91
Afif718 <39927237+Afif718@users.noreply.github.com>
2025-07-31 20:26:17 +0600

    Updated at 2025-07-31 20:26:16

---
+++ 3_Basic_Data_Structures/29_Module_19.5_Practice_Day_2/1_root_equals_to_sum.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if(root == NULL){
+            return false;
+        }
+
+        int root_val = root->val;
+        
+
+        int l = root->left ? root->left->val : 0;
+        int r = root->left ? root->right->val : 0;
+
+        return root_val == l+r;
+    }
+};
+
+/*
+https://leetcode.com/problems/root-equals-sum-of-children/description/
+
+
+Code
+
+
+Testcase
+Test Result
+Test Result
+2236. Root Equals Sum of Children
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: root = [10,4,6]
+Output: true
+Explanation: The values of the root, its left child, and its right child are 10, 4, and 6, respectively.
+10 is equal to 4 + 6, so we return true.
+Example 2:
+
+
+Input: root = [5,3,1]
+Output: false
+Explanation: The values of the root, its left child, and its right child are 5, 3, and 1, respectively.
+5 is not equal to 3 + 1, so we return false.
+ 
+
+Constraints:
+
+The tree consists only of the root, its left child, and its right child.
+*/
